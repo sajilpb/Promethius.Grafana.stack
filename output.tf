b9f27551ae701204f34024d7aac9724466394058
sajil <sajil>
2025-11-25 22:05:03 +0530

    update

--- output.tf
+++
@@ -1,5 +0,0 @@
-output "grafana_admin_password" {
-  description = "Grafana admin password"
-  value       = replace(base64decode(data.kubernetes_secret.grafana.data["admin-password"]), "\n", "")
-  sensitive   = true
-}
